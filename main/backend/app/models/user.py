7d730f8df22f47ec802b330e265ba9d15f73f9cc
Alisson Ninomiya <53145093+alisnino@users.noreply.github.com>
2024-03-23 18:58:31 +0900

    User table update

--- main/backend/app/models/user.py
+++ main/backend/app/models/user.py
@@ -2,6 +2,6 @@ from db import db
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  cognito_id = db.Column(db.String(128))
+  username = db.Column(db.String(128), unique=True, nullable=False)
   display_name = db.Column(db.String(128))
   __tablename__ = 'users'
